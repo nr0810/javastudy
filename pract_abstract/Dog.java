1a38989317ffcd850be20576f4a4941b716b3698
NR <nr970810@gmail.com>
2019-02-23 13:59:30 +0900

    [practice abstract]

---
+++ pract_abstract/Dog.java
@@ -0,0 +1,13 @@
+package pract_abstract;
+
+public class Dog extends Animal {
+	public Dog() {
+		this.kind = "Æ÷À¯·ù";
+	}
+	
+	@Override
+	public void sound() {
+		 System.out.println("¸Û¸Û");
+	}
+
+}
